df03217f41e8c73b3f28260f45772337dd9ec452
HafizZiq <hafizuddinismail552@gmail.com>
2021-05-04 02:04:45 +0800

    build.sh: update
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- build.sh
+++ build.sh
@@ -6,11 +6,10 @@ PARENT_DIR="$(dirname "$KERNEL_DIR")"
 KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
 export KBUILD_BUILD_USER="hafizziq"
 export KBUILD_BUILD_HOST="ubuntu"
-# export KBUILD_BUILD_VERSION="69"
 export KBUILD_BUILD_TIMESTAMP="$(TZ=Asia/Kuala_Lumpur date)"
 export PATH="$HOME/clang/proton/bin:$PATH"
 export LD_LIBRARY_PATH="$HOME/clang/proton/lib:$LD_LIBRARY_PATH"
-export KBUILD_COMPILER_STRING="$($HOME/clang/proton/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
+export KBUILD_COMPILER_STRING="$($HOME/clang/proton/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export CROSS_COMPILE=$HOME/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=$HOME/gcc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export out=out
@@ -24,42 +23,108 @@ fi;
 mkdir -p $out
 
 # Functions
-clang_build () {
-    make -j$(nproc --all) O=$out \
-                          ARCH=arm64 \
-                          CC="clang" \
-                          AR="llvm-ar" \
-                          NM="llvm-nm" \
-                          LD="ld.lld" \
-                          AS="llvm-as" \
-                          OBJCOPY="llvm-objcopy" \
-                          OBJDUMP="llvm-objdump" \
-                          CLANG_TRIPLE=aarch64-linux-gnu- \
-                          CROSS_COMPILE=$CROSS_COMPILE \
-                          CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32
+# sticker plox
+function sticker() {
+curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+-d sticker="CAACAgUAAxkBAAECQa5gj_CEuD-DSeBNVc4xkVxLTQF6zQACmwEAAvQneFfl-dS0RNs7CR8E" \
+-d chat_id=$chat_id
 }
 
-# Build kernel
+# Send info plox channel
+function sendinfo() {
+curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+-d chat_id="$chat_id" \
+-d "disable_web_page_preview=true" \
+-d "parse_mode=html" \
+-d text="<b>• SixTeen Krumel •</b>%0ABuild started on <code>Ur bed</code>%0AFor device <b>Xiaomi Redmi Note 8/T</b>%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#$STS"
+}
+
+# Fin Error
+function finerr() {
+curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+-d sticker="CAACAgUAAxkBAAECQbBgj_TChQX9TxDwjznYULukxzn3FQACQQQAAp3uyyd8NONSuiPU3x8E" \
+-d chat_id=$chat_id
+curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+-d chat_id="$chat_id" \
+-d "disable_web_page_preview=true" \
+-d "parse_mode=markdown" \
+-d text="Build throw an error(s)"
+exit 1
+}
+
+# Compile plox
+function clang_build () {
+echo -e ""
+START=$(date +"%s")
 make O=$out ARCH=arm64 $CONFIG > /dev/null
 echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
 echo -e "\nCompiling $ZIPNAME\n"
-clang_build
-if [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "$out/arch/arm64/boot/dtbo.img" ]; then
- echo -e "\nKernel compiled succesfully! Zipping up...\n"
- ZIPNAME="SixTeen•Kernel•R•Ginklow-$(date '+%Y%m%d-%H%M').zip"
- if [ ! -d AnyKernel3 ]; then
-  git clone -q https://github.com/HafizZiq/AnyKernel3 --depth=1 -b sixteen
- fi;
- cp -f $out/arch/arm64/boot/Image.gz-dtb AnyKernel3
- cp -f $out/arch/arm64/boot/dtbo.img AnyKernel3
- cd AnyKernel3
- zip -r9 "$HOME/$ZIPNAME" *
- cd ..
- rm -rf AnyKernel3
- echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
- echo -e "Zip: $ZIPNAME\n"
- rm -rf $out
-else
- echo -e "\nCompilation failed!\n"
+make -j$(nproc --all) O=$out \
+ARCH=arm64 \
+CC="clang" \
+AR="llvm-ar" \
+NM="llvm-nm" \
+LD="ld.lld" \
+AS="llvm-as" \
+OBJCOPY="llvm-objcopy" \
+OBJDUMP="llvm-objdump" \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=$CROSS_COMPILE \
+CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32
+END=$(date +"%s")
+if ! [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && ! [ -f "$out/arch/arm64/boot/dtbo.img" ]; then
+ finerr
+ exit 1
+fi
+}
+
+# Zipping
+function zipping() {
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+ZIPNAME="SixTeen•Kernel•R•Ginklow-$(date '+%Y%m%d-%H%M').zip"
+if [ ! -d AnyKernel3 ]; then
+ git clone https://github.com/HafizZiq/AnyKernel3 --depth=1 -b sixteen
 fi;
+cp -f $out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+cp -f $out/arch/arm64/boot/dtbo.img AnyKernel3
+cd AnyKernel3
+zip -r9 "$HOME/$ZIPNAME" *
+cd ..
+rm -rf AnyKernel3
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo -e "Zip: $ZIPNAME\n"
+}
+
+# Push kernel
+function push() {
+curl -F document=@$HOME/$ZIPNAME "https://api.telegram.org/bot$token/sendDocument" \
+-F chat_id="$chat_id" \
+-F "disable_web_page_preview=true" \
+-F "parse_mode=html" \
+-F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)."
+}
+
+sticker
+sendinfo
+clang_build
+zipping
+DIFF=$(($END - $START))
+while read -p "Upload? (y/n)" bchoice; do
+case "$bchoice" in
+ n|N)
+  break
+ ;;
+ y|Y)
+  push
+  break
+ ;;
+ *)
+  echo
+  echo "Try again please!"
+  echo
+ ;;
+esac
+done
+rm -rf $out
+echo -e ""
 exit 0
